0bedd9731e35ba984060e41c01fb8a5ac1e5f1e1
Mirave <potapovnikita72@gmail.com>
2023-03-10 19:04:35 +0300

    Lab1

--- Desktop/Window1.xaml.cs
+++ Desktop/Window1.xaml.cs
@@ -10,29 +10,28 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Desktop
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
         }
         private void SignUp(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Некуда заходить(");
+            MessageBox.Show("Создана новая учётная запись");
         }
 
-        private void registration(object sender, RoutedEventArgs e)
+        private void Back(object sender, RoutedEventArgs e)
         {
-            Window1 reg = new Window1();
-            reg.Show();
+            MainWindow main = new MainWindow();
+            main.Show();
             Close();
         }
     }
